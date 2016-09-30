4f401021c1590d4dc166f7ca23ca5f7ac18e3011
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2016-09-30 17:14:40 -0500

    no message

---
+++ angularm4hw/scripts/menudata.service.js
@@ -0,0 +1,27 @@
+(function() {
+	'use strict';
+
+	angular.module('data')
+		.service('MenuDataService', MenuDataService)
+		.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+	MenuDataService.$inject = [];
+	function MenuDataService() {
+		var service = this;
+		
+		service.getAllCategories = function () {
+			return $http({
+				method: "GET",
+				url: (ApiBasePath + '/categories.json')
+			});
+		};
+
+		service.getItemsForCategory = function(categoryShortName) {
+			return $http({
+				method: "GET",
+				url: (ApiBasePath + '/menu_items.json'),
+				params: { category: categoryShortName }
+			});
+		};
+	}
+})():
